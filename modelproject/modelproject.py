15865d723561f199d1a60075c4e70309582d8ed1
Fxc322$ <fxc322@alumni.ku.dk>
2023-05-22 14:33:43 +0200

    Finitooo

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,54 +1,87 @@
-import numpy as np
+# Importing packages
 from scipy import optimize
-import sympy as sm
-from sympy import *
-from types import SimpleNamespace
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class ModelProjectClass:
-    
-    def __init__(self, n):
-        """ setup model """
+    def cournotmodel(self, n, a, b, c):
+        """ setup cournot model"""
+        # Making a list to hold the quantities produced for each firm
+        q_liste = np.ones(n)
 
-        # a. creating namespaces
-        self.par = SimpleNamespace()
+        # Creating the demand function
+        def demand(q_liste, a, b):
+            return a - np.matmul(b * np.ones(len(q_liste)), q_liste)
 
-        # b. defining the cost function
-    def cost(self, q):
-        return self.par.c * np.sum(q)
-    #self.cost = c
+        # Creating the profit function
+        def profit(q_liste, firm, a, b, c):
+            return (demand(q_liste, a, b) - c) * q_liste[firm]
 
-    # c. dDefininf the revenue
-    def rev(self,p):
-        # Imposing restrictions that the price cannot be negative
-        if p >= 0:
-            return p * self.q(p)
-        else:
-            return -np.inf
-    
+        # Creating the profit max function
+        def profitmax(q_liste, firm, a, b, c):
+            def opt_response(q_new):
+                q_liste_new = q_liste.copy()
+                q_liste_new[firm] = q_new
+                return -profit(q_liste_new, firm, a, b, c)
 
+        # Each firms optimal response (in quantities)
+            q_opt_response = optimize.brute(opt_response, ((0, 100,),))
 
-    # defining the inverse demand funtion
-    def q(self, p):
-        
-        return (self.par.a - p) / self.par.b / self.par.n
-        
+            return q_opt_response
 
-    def EQ(self, a, b, c, n):
-        """ Finding equilibrium for all firms """
+        # Creating best response
+        def profitmax_vector(q_liste, a, b, c):
+            
+            # List to store each firms BR
+            BR = []
 
-        # a. setting parameters
-        self.par.a = a
-        self.par.b = b
-        self.par.c = c
-        self.par.n = n
+            # Iterting over each firm and append in the BR list
+            for firm in range(len(q_liste)):
+                BR.append(profitmax(q_liste, firm, a, b, c))
 
-        # b. optimal EQ amounts given the optimal price
-        initial_guess = np.ones(n) / n
-        p_opt = optimize.minimize(lambda p: -self.rev(p) + self.cost(np.ones(n) * self.q(p)), x0=self.par.a, bounds=[(0, None)])
-        
-        sol_q = np.maximum(self.q(p_opt.x) * n, 0)
-        sol_p = self.par.a - self.par.b * sol_q * self.par.n
- 
-        return sol_q, sol_p, self.par.a, self.par.b, self.par.c, self.par.n
+            # Converting thew list to a 1-dimensional array
+            BR = np.array(BR).reshape(-1)
+
+            return q_liste - BR
+
+        # Optimizing to find the optimal equilibrium quantities
+        EQ = optimize.fsolve(profitmax_vector, q_liste, args=(a, b, c))
+
+        return EQ
+
+    def Results(self, n, a, b, c):
+        # Making empty lists to store the results
+        EQ_list = []
+        n_list = []
+
+        # Loop over n firms and appending the results
+        for n_val in range(2, n + 1):
+            n_list.append(n_val)
+
+            # Finding the optimal quantities responding to n number of firms and appending the results
+            EQ_iter = self.cournotmodel(n_val, a, b, c)
+            EQ_list.append(EQ_iter[0])
+
+        return EQ_list, n_list
+
+    # Set up for the bar plot
+    def plot1(self, a, b, c):
+        n = 2
+        EQ = self.cournotmodel(n, a, b, c)
+
+        plt.bar(range(n), EQ)
+        plt.xlabel("Firms")
+        plt.ylabel("Optimal Quantity")
+        plt.title("Optimal Quantities for Two Firms")
+        plt.xticks(range(n), ['Firm 1', 'Firm 2'])
+        plt.ylim(0, max(EQ) * 1.2)
+        plt.show()
+
+    # Set up for the line plot
+    def plot2(self, EQ_list, n_list):
+        plt.plot(n_list, EQ_list)
+        plt.xlabel("N Number of Firms")
+        plt.ylabel("Optimal Quantity")
+        plt.title("Optimal Quantities for N Numbers of Firms")
+        plt.show()
